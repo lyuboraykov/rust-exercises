e0d3e747ce86201041643e37b116eadf792c6aaf
lyuboraykov <lyubo.raykov@gmail.com>
2017-10-09 22:23:58 +0200

    references

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 fn main() {
-    const N: u64 = 400;
+    let mut s = String::from("this is a string");
+    takes_a_reference(&mut s);
 
-    let mut first: u64 = 0;
-    let mut second: u64 = 1;
+    println!("{}", s);
+}
 
-    for _ in 1..N {
-        let temp: u64 = second;
-        second = first + second;
-        first = temp;
-    }
-
-    println!("Result is {}", second);
+fn takes_a_reference(s: &mut String) {
+    s.push_str(" this is a pushed string");
 }
