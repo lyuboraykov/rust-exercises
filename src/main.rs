5e0094bed70ffc540ae181029db498a290fbe780
lyuboraykov <lyubo.raykov@gmail.com>
2017-10-10 22:51:47 +0200

    string slices

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let mut s = String::from("this is a string");
-    takes_a_reference(&mut s);
-
-    println!("{}", s);
+    let s = String::from("this is a string");
+    println!("{}", get_first_n_words(&s, 2));
 }
 
-fn takes_a_reference(s: &mut String) {
-    s.push_str(" this is a pushed string");
+fn get_first_n_words(s: &str, n: u32) -> &str {
+    let bytes = s.as_bytes();
+    let mut count = 0;
+    for (i, &byte) in bytes.iter().enumerate() {
+        if byte == b' ' {
+            count += 1;
+        }
+        if count == n {
+            return &s[..i];
+        }
+    }
+    &s
 }
