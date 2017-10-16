347501317543b92108b711aa61c4bd365c16ea7a
lyuboraykov <lyubo.raykov@gmail.com>
2017-10-16 22:45:03 +0200

    Add a module

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-use std::collections::LinkedList;
 
-struct MetricHolder {
-    n: usize,
-    sum: u32,
-    history: LinkedList<u32>
-}
+mod metrics {
+    use std::collections::LinkedList;
 
-impl MetricHolder {
-    fn moving_average(&mut self, new_val: u32) -> f64 {
-        self.history.push_back(new_val);
-        self.sum += new_val;
-        if self.history.len() > self.n {
-            if let Some(val) = self.history.pop_front() {
-                self.sum -= val;
+    pub struct MetricHolder {
+        n: usize,
+        sum: u32,
+        history: LinkedList<u32>
+    }
+
+    impl MetricHolder {
+        pub fn moving_average(&mut self, new_val: u32) -> f64 {
+            self.history.push_back(new_val);
+            self.sum += new_val;
+            if self.history.len() > self.n {
+                if let Some(val) = self.history.pop_front() {
+                    self.sum -= val;
+                }
+            }
+
+            self.sum as f64 / self.history.len() as f64
+        }
+
+        pub fn new(n: usize) -> MetricHolder {
+            MetricHolder {
+                n: n,
+                sum: 0,
+                history: LinkedList::new()
             }
         }
-
-        self.sum as f64 / self.history.len() as f64
-    }
-
-    fn new(n: usize) -> MetricHolder {
-        MetricHolder {
-            n: n,
-            sum: 0,
-            history: LinkedList::new()
-        }
     }
 }
 
+
 fn main() {
-    let mut metric_holder = MetricHolder::new(2);
+    let mut metric_holder = metrics::MetricHolder::new(2);
     println!("{}", metric_holder.moving_average(1));
     println!("{}", metric_holder.moving_average(1));
     println!("{}", metric_holder.moving_average(1));
