b0d100e380b98255c30507fde17e8d89511d1fcf
lyuboraykov <lyubo.raykov@gmail.com>
2017-10-17 22:49:53 +0200

    split into two files

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,4 @@
-
-mod metrics {
-    use std::collections::LinkedList;
-
-    pub struct MetricHolder {
-        n: usize,
-        sum: u32,
-        history: LinkedList<u32>
-    }
-
-    impl MetricHolder {
-        pub fn moving_average(&mut self, new_val: u32) -> f64 {
-            self.history.push_back(new_val);
-            self.sum += new_val;
-            if self.history.len() > self.n {
-                if let Some(val) = self.history.pop_front() {
-                    self.sum -= val;
-                }
-            }
-
-            self.sum as f64 / self.history.len() as f64
-        }
-
-        pub fn new(n: usize) -> MetricHolder {
-            MetricHolder {
-                n: n,
-                sum: 0,
-                history: LinkedList::new()
-            }
-        }
-    }
-}
-
+mod metrics;
 
 fn main() {
     let mut metric_holder = metrics::MetricHolder::new(2);
