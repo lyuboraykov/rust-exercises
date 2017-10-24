894f3d3ae44039b6ad3f18ad10676ddfc09f4e6e
lyuboraykov <lyubo.raykov@gmail.com>
2017-10-24 20:52:02 +0200

    Add a simple rate limiter

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
 extern crate metrics;
 
+use std::{time, thread};
+
+use metrics::metric_holder::MetricHolder;
+use metrics::rate_limiter::{AcquireResult, RateLimiter};
+
+
 fn main() {
-    let mut metric_holder = metrics::MetricHolder::new(2);
+    let mut metric_holder = MetricHolder::new(2);
+    let mut moving_average_rate_limiter = RateLimiter::new(1, 10);
+
+    for _ in 0..12 {
+        match moving_average_rate_limiter.acquire() {
+            AcquireResult::Free => {
+                println!("Free to go")
+            },
+            AcquireResult::Limited => {
+                println!("Rate limited, sorry")
+            },
+        }
+    }
+
+    thread::sleep(time::Duration::from_millis(1001));
+
+    for _ in 0..12 {
+        match moving_average_rate_limiter.acquire() {
+            AcquireResult::Free => {
+                println!("Free to go")
+            },
+            AcquireResult::Limited => {
+                println!("Rate limited, sorry")
+            },
+        }
+    }
+
     println!("{}", metric_holder.moving_average(1));
     println!("{}", metric_holder.moving_average(1));
     println!("{}", metric_holder.moving_average(1));
---
+++ src/rate_limiter.rs
@@ -0,0 +1,45 @@
+use std::collections::LinkedList;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::cmp::Ordering;
+
+pub enum AcquireResult {
+    Limited,
+    Free,
+}
+
+pub struct RateLimiter {
+    call_times: LinkedList<u64>,
+    window_size: u64, // seconds
+    limit: u32
+}
+
+impl RateLimiter {
+    pub fn acquire(&mut self) -> AcquireResult {
+        let now_time = SystemTime::now();
+        let now_seconds = now_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
+        self.call_times.push_back(now_seconds);
+        let mut redundant_calls_count = 0;
+        for call_time in self.call_times.iter() {
+            if now_seconds - call_time >= self.window_size {
+                redundant_calls_count += 1;
+            } else {
+                break;
+            }
+        }
+        for _ in 0..redundant_calls_count {
+            self.call_times.pop_front();
+        }
+        return match self.call_times.len().cmp(&(self.limit as usize)) {
+            Ordering::Greater => AcquireResult::Limited,
+            _ => AcquireResult::Free
+        };
+    }
+
+    pub fn new(window_size: u64, limit: u32) -> RateLimiter {
+        return RateLimiter {
+            window_size: window_size,
+            limit: limit,
+            call_times: LinkedList::new()
+        }
+    }
+}
